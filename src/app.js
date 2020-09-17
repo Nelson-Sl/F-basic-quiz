420f7c19866a876c227587d669172f2ff45a4513
杜娟 <jdu.xian@gmail.com>
2020-09-17 08:51:50 +0800

    feedback from Du Juan

--- src/app.js
+++ src/app.js
@@ -2,11 +2,13 @@ import "./style/index.scss";
 import $ from "jquery";
 import { userUrl } from "./js/fetchAddress";
 
+// TODO feedback: 不建议用jquery立即调用，定义方法，然后方法调用就可以了，不然也很难测试
 $(function () {
   fetch(userUrl)
     .then((response) => response.json())
     .then((data) => {
       // user.education = data;
+      // TODO feedback: 不建议用标签选择器，未来新添加的标签都会被写入src
       $("img").attr("src", data.avatar);
       $(".resume-author").text(data.name);
       $(".resume-author-age").text(data.age);
--- src/js/fetchAddress.js
+++ src/js/fetchAddress.js
@@ -1,4 +1,5 @@
 const userUrl = "http://localhost:3000/users/1";
 const userEducationUrl = "http://localhost:3000/users/1/educations";
 
+// TODO feedback: 刚开始的时候需要为常量单独抽文件，当常量多了后可以考虑。有些过度设计
 export { userUrl, userEducationUrl };
